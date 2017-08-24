87c35193aa2c226864e740b9bc398eda8efee33a
shenchuang <shenc@getui.com>
2017-08-24 15:03:11 +0800

    change component radio-group

--- src/components/RadioGroup/index.vue
+++ src/components/RadioGroup/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="radio-group">
-    <span>123123123</span>
+    <div v-for="item in list" :key="item.value" class="radio-item">
+      <input
+        type="radio"
+        value="{{ item.value }}"
+        id="{{ item.label + '-' + item.value }}"
+        name="{{ 'radio-group' + random }}"
+        v-model="selectedValue"
+        class="hide"
+      />
+      <label
+        for="{{ item.label + '-' + item.value }}"
+        class="radio-label"
+        :class="{
+          'radio-label-last': $index === list.length - 1,
+          'radio-label-first': $index === 0,
+          'radio-label-active': selectedValue === item.value
+        }"
+      >
+        {{ item.label }}
+      </label>
+    </div>
   </div>
 </template>
 
@@ -9,16 +29,22 @@
 export default {
 
   props: {
-    list: { type: Array, required: true }
+    list: { type: Array, required: true },
+    selectedValue: {
+      default: ''
+    }
   },
 
-  data: () => ({}),
+  data: () => ({
+    random: Math.floor(Math.random * 1000)
+  }),
 
   computed: {
 
   },
   attached () {
-    console.log(this.list)
+    console.log(this.list, this.selectedValue)
+    if (this.selectedValue === '' && this.list.length) this.selectedValue = this.list[0].value
   }
 
 }
--- src/views/notebook/functionBar.vue
+++ src/views/notebook/functionBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="funcion-bar">
     <button class="btn add-new-notebook">New Notebook</button>
-    <radio-group list="showTypes"></radio-group>
+    <radio-group :list="orderTypes" class="order-types" :selected-value="orderType"></radio-group>
+    <radio-group :list="layoutTypes" class="layout-types" :selected-value="layoutType"></radio-group>
   </div>
 </template>
 <script>
@@ -12,7 +13,17 @@ export default {
   components: { RadioGroup },
 
   data: () => ({
-    showTypes: ['Name', 'Count', 'Updated']
+    orderTypes: [
+      { label: 'Name', value: 'name' },
+      { label: 'Count', value: 'count' },
+      { label: 'Updated', value: 'updated' }
+    ],
+    orderType: 'name',
+    layoutTypes: [
+      { label: 'icon', value: 'icon' },
+      { label: 'list', value: 'list' }
+    ],
+    layoutType: 'list'
   })
 }
 </script>
