ecd68ec7c7034f018055dbff839ae324c0e30924
shenchuang <shenc@getui.com>
2017-09-26 17:39:24 +0800

    repace $.deferred with es6 Promise

--- src/services/xhr/axios.js
+++ src/services/xhr/axios.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 const rootPath = '/api' // 后端 API 根路径
 
 const xhr = ({ method = 'get', url, body = null }) => {
-  // 由于引入了 es6-shim，因此这里完全可以使用原生 Promise
-  const defer = $.Deferred()
 
   const queryString = data => {
     let str = ''
@@ -15,21 +13,41 @@ const xhr = ({ method = 'get', url, body = null }) => {
     return str
   }
 
-  axios({
-    method: method,
-    url: rootPath + url + (method === 'get' ? queryString(body) : ''),
-    data: body
-  }).then(res => {
-    const { status, data } = res
-    if (status !== 200) {
-      return defer.reject(data)
-    }
-    defer.resolve(data.data)
-  }).catch(err => {
-    console.log(err, 2)
+  const promise = new Promise((resolve, reject) => {
+    axios({
+      method: method,
+      url: rootPath + url + (method === 'get' ? queryString(body) : ''),
+      data: body
+    }).then(res => {
+      const { status, data } = res
+      if (status !== 200) {
+        return reject(data)
+      }
+      resolve(data.data)
+    }).catch(err => {
+      console.log(err, 2)
+    })
   })
 
-  return defer.promise()
+  return promise
+
+  // const defer = $.Deferred()
+
+  // axios({
+  //   method: method,
+  //   url: rootPath + url + (method === 'get' ? queryString(body) : ''),
+  //   data: body
+  // }).then(res => {
+  //   const { status, data } = res
+  //   if (status !== 200) {
+  //     return defer.reject(data)
+  //   }
+  //   defer.resolve(data.data)
+  // }).catch(err => {
+  //   console.log(err, 2)
+  // })
+
+  // return defer.promise()
 }
 
 export default xhr
