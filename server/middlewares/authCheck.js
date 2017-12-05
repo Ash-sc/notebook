c512fc449177a942f3731181be8cae41391b5d82
shenchuang <shenc@getui.com>
2017-12-05 11:16:48 +0800

    add express-sesson

--- server/middlewares/authCheck.js
+++ server/middlewares/authCheck.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-  if (!req.cookies.userId) {
-    res.status(400).json({
+  if (!req.session.sessionInfo) {
+    res.status(200).json({
       success: false,
       errorMsg: 'not login!'
     })
